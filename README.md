17a0ac0ede02883f711d9008f474adddc2438a27
Pigeon <kara.graysen@gmail.com>
2024-01-21 21:46:23 -0800

    Add initial Axum hello world application/configuration (#1)

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Backpack
 
 Backpack, Backpack. :notes:
+
+## Getting Started
+
+1. Clone the repository.
+2. Run `source .env` from root directory.
+3. Run `cargo run` from root directory.
+4. Hit the address using Postman.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, process};
+use axum::{
+    routing::get,
+    Router,
+};
+
+#[tokio::main]
+async fn main() {
+    // TODO: Replace 'Hello World' route with real implementation.
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let axum_port = match env::var("BACKPACK_AXUM_PORT") {
+        Ok(unwrapped_axum_port) => {
+            println!("Binding Axum to port assigned in the .env: {}", unwrapped_axum_port);
+            unwrapped_axum_port
+        },
+        Err(error) => {
+            println!("Axum port was not assigned. Run source .env from root. Error: {}", error);
+            process::abort();
+        },
+    };
+    let formatted_address = format!("0.0.0.0:{}", axum_port);
+    let listener = tokio::net::TcpListener::bind(formatted_address).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
