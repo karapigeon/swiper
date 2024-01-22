60148e7a8d79b83657ad63004016ea152a2c1337
Kara Graysen <kara.graysen@gmail.com>
2024-01-21 21:45:06 -0800

    Add .env handling and bindable address

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::{env, process};
 use axum::{
     routing::get,
     Router,
@@ -5,10 +6,20 @@ use axum::{
 
 #[tokio::main]
 async fn main() {
-    // build our application with a single route
+    // TODO: Replace 'Hello World' route with real implementation.
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
-    // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let axum_port = match env::var("BACKPACK_AXUM_PORT") {
+        Ok(unwrapped_axum_port) => {
+            println!("Binding Axum to port assigned in the .env: {}", unwrapped_axum_port);
+            unwrapped_axum_port
+        },
+        Err(error) => {
+            println!("Axum port was not assigned. Run source .env from root. Error: {}", error);
+            process::abort();
+        },
+    };
+    let formatted_address = format!("0.0.0.0:{}", axum_port);
+    let listener = tokio::net::TcpListener::bind(formatted_address).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
